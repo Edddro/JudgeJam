366de8a692ee540e28327dd24455c191db0c3242
Anipaleja <anipaleja@gmail.com>
2025-05-18 01:54:46 -0400

    Delete frontend directory
    Deleting to make room for the duplicated front to backend files

--- frontend/app/judge/page.tsx
+++
@@ -1,9 +0,0 @@
-import JudgeView from "@/components/judge-view"
-
-export default function JudgePage() {
-  return (
-    <main className="min-h-screen bg-light-blue">
-      <JudgeView />
-    </main>
-  )
-}
--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import JudgeJamInterface from "@/components/judge-jam-interface"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-light-blue">
-      <JudgeJamInterface />
-    </main>
-  )
-}
